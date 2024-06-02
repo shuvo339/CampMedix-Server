5d07a9cd854a5307a074a1f397bf80a4b387ed5b
shuvo339 <shubhroshafiullah@gmail.com>
2024-06-02 20:15:57 +0600

    get camp data from database

--- index.js
+++ index.js
@@ -57,7 +57,20 @@ async function run() {
 
 
     //camps related api
-   
+    app.get('/camps', async(req,res)=>{
+      const search = req.query.search || '';
+      const sort = req.query.sort || 'fees';
+      console.log(search);
+      const query = search ? { campName: { $regex: search, $options: 'i' } } : {}
+      const options = {
+        // Sort returned documents in ascending order by title (A->Z)
+        sort: { [sort]: 1 },
+        
+      };
+      const result = await campsCollection.find().toArray();
+      res.send(result)
+    })
+  
 
 
     // Send a ping to confirm a successful connection
