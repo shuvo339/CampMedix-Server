6132f9649e030738339c9f32493bb161c01d9fec
shuvo339 <shubhroshafiullah@gmail.com>
2024-06-03 21:32:52 +0600

    user data post and get

--- index.js
+++ index.js
@@ -27,33 +27,76 @@ const client = new MongoClient(uri, {
   }
 });
 
-//middleware
-// const verifyToken = async (req, res, next) => {
-//   const token = req.cookies?.token;
-//   console.log('token in middleware', token)
-//   if (!token) {
-//       return res.status(401).send({ message: 'unauthorized access' })
-//   }
-//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-//       if (err) {
-//         console.log(err)
-//           return res.status(401).send({ message: 'unauthorized access' })
-//       }
-//       console.log('docoded', decoded)
-//       req.user = decoded;
-//       next();
-//   })
-// }
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     const campsCollection = client.db('campsDB').collection('camps');
+    const usersCollection = client.db('campsDB').collection('users');
+    const registrationsCollection = client.db('campsDB').collection('registrations');
 
-    //jwt token generate
+        //jwt related api
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      console.log('token test', token );
+      res.send({ token })
+    })
+
+    // middlewares 
+    const verifyToken = (req, res, next) => {
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access' });
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+      })
+    }
     
-    //clearing Token
+
+    //user role entry related api
+  app.put('/user', async (req, res) => {
+      const user = req.body
+
+      const query = { email: user?.email }
+      // check if user already exists in db
+      const isExist = await usersCollection.findOne(query)
+      // if (isExist) {
+      //   if (user.status === 'Requested') {
+      //     // if existing user try to change his role
+      //     const result = await usersCollection.updateOne(query, {
+      //       $set: { status: user?.status },
+      //     })
+      //     return res.send(result)
+      //   } else {
+      //     // if existing user login again
+      //     return res.send(isExist)
+      //   }
+      // }
+
+      // save user for the first time
+      const options = { upsert: true }
+      const updateDoc = {
+        $set: {
+          ...user,
+        },
+      }
+      const result = await usersCollection.updateOne(query, updateDoc, options)
+     
+      res.send(result)
+    })
+
+    app.get('/users', async(req,res)=>{
+      const result = await usersCollection.find().toArray();
+      res.send(result)
+    })
+
 
 
     //camps related api
@@ -91,6 +134,23 @@ async function run() {
       const result = await campsCollection.insertOne(camp);
       res.send(result); 
     })
+
+
+      //   app.patch('/participant/:id', async (req, res) => {
+      // const id = req.params.id;
+      // console.log(id);
+      // const filter = { _id: new ObjectId(id) }
+      // const updateDoc ={
+      //   $set: {
+      //     "participant": parseInt()
+          
+      //   }
+      // }
+    //  const result = await campsCollection.updateOne(filter, {$inc: {participant: 1}})
+    // res.send(result);
+    // })
+
+    // registration related api 
 
 
     // Send a ping to confirm a successful connection
