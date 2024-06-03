9cd174e86fce438af2d850cc2fd32a5a691295c3
shuvo339 <shubhroshafiullah@gmail.com>
2024-06-03 23:05:08 +0600

    delete registration applied

--- index.js
+++ index.js
@@ -168,6 +168,13 @@ async function run() {
       res.send(result);
     })
 
+    app.delete('/register/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await registrationsCollection.deleteOne(query);
+      res.send(result); 
+    })
+
 
 
     // Send a ping to confirm a successful connection
